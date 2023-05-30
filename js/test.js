e0c35cf20d5885cceef01bac2bffc9145940ce08
young <55731744@qq.com>
2023-05-30 17:21:45 +0800

    更改点交互

--- js/test.js
+++ js/test.js
@@ -1,1 +1,11 @@
-window.alert('张姿怡很漂亮')
+(function () {
+    var flag1 = window.confirm('喜欢我吗？喜欢点确定，不喜欢点取消')
+    if (!flag1) {
+       var flag2 = window.confirm('真的不喜欢吗？')
+        if (!flag2) {
+            window.alert('好啦，不管喜欢不喜欢都给你看')
+        }
+    }
+    let canvas = document.getElementById('pinkboard')
+    canvas.style.opacity = '1'
+})()
