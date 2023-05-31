38962a3f09b65a5251a91ba5088079c2cb32e036
young <55731744@qq.com>
2023-05-31 14:58:12 +0800

    测试逻辑

--- js/test.js
+++ js/test.js
@@ -1,13 +1,16 @@
 (function () {
     var flag1 = window.confirm('你喜欢我吗？喜欢点确定，不喜欢点取消')
     if (!flag1) {
-       var flag2 = window.confirm('真的不喜欢吗？喜欢点确定，不喜欢点取消')
+        var flag2 = window.confirm('真的不喜欢吗？喜欢点确定，不喜欢点取消')
         if (!flag2) {
             window.alert('好啦，不管喜欢不喜欢都给你看')
         }
     }
     let canvas = document.getElementById('pinkboard')
-    let player = document.getElementById('players')
     canvas.style.opacity = '1'
-    player.play()
+    function doPlay() {
+        let player = document.getElementById('players')
+        player.play()
+    }
+    document.addEventListener('click', doPlay)
 })()
