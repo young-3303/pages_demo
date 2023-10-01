599b6e3f7f621ea9494f3aae6e32d36ac6692ae3
young <55731744@qq.com>
2023-10-01 21:05:15 +0800

    修改交互

--- js/test.js
+++ js/test.js
@@ -1,21 +1,13 @@
 (function () {
-    var flag1 = window.confirm('你喜欢我吗？喜欢点确定，不喜欢点取消')
-    if (!flag1) {
-        var flag2 = window.confirm('真的不喜欢吗？喜欢点确定，不喜欢点取消')
-        if (!flag2) {
-            window.alert('好啦，不管喜欢不喜欢都给你看')
+    const msg = window.prompt('请输入我的全名哦')
+    if (!msg) return false
+    else {
+        if (msg !== "杨梦舟") {
+            alert('哎哟回答错误，再来')
+            location.reload();
+            return false
         }
+
     }
-    let canvas = document.getElementById('pinkboard')
-    canvas.style.opacity = '1'
-    function doPlay(){
-        let player = document.getElementById('players')
-        player.play()
-    }
-    // setTimeout(() => {
-    //     player.pause()
-    //     player.play()
-    // }, 0)
-    document.addEventListener('click', doPlay)
-    document.addEventListener('mouseenter', doPlay)
-})()
+    init(document.getElementById('pinkboard'))
+})();
